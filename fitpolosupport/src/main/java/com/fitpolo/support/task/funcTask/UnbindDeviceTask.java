53201234946dc93572883eb5f8e924cdce806fd9
rohn <878443170@qq.com>
2024-08-02 14:34:06 +0800

    feat：增加解绑手表指令

--- fitpolosupport/src/main/java/com/fitpolo/support/task/funcTask/UnbindDeviceTask.java
+++ fitpolosupport/src/main/java/com/fitpolo/support/task/funcTask/UnbindDeviceTask.java
@@ -13,35 +13,26 @@ import com.fitpolo.support.utils.DigitalConver;
 import java.util.Arrays;
 
 /**
- * @Date 2024/7/31
- * @Author luoming
- * @Description 同步时间
- * @ClassPath com.fitpolo.support.task.SyncTimeTask
+ * 同步时间
  */
 public class UnbindDeviceTask extends OrderTask {
 
-    private static final int ORDERDATA_LENGTH = 14;
+    private static final int ORDERDATA_LENGTH = 8;
 
     private byte[] orderData;
 
     public UnbindDeviceTask (MokoOrderTaskCallback callback) {
-        super(OrderType.WRITE, OrderEnum.syncTime, callback, OrderTask.RESPONSE_TYPE_WRITE_NO_RESPONSE);
+        super(OrderType.WRITE, OrderEnum.unbindDevice, callback, OrderTask.RESPONSE_TYPE_WRITE_NO_RESPONSE);
         orderData = new byte[ORDERDATA_LENGTH];
         orderData[0] = (byte) MokoConstants.HEADER_READ_SEND;
-        orderData[1] = (byte) 0x0C;
+        orderData[1] = (byte) 0x06;
         orderData[2] = (byte) 0x02;
         orderData[3] = (byte) order.getOrderHeader();
-        orderData[4] = (byte) 0x07;
-        orderData[5] = (byte) 0x18;
-        orderData[6] = (byte) 0x07;
-        orderData[7] = (byte) 0x1F;
-        orderData[8] = (byte) 0x12;
-        orderData[9] = (byte) 0x06;
-        orderData[10] = (byte) 0x05;
-        orderData[11] = (byte) 0x03;
-        orderData[12] = (byte) 0xFF;
-        orderData[13] = (byte) 0xFF;
-//        [255, 12, 2, 1, 7, 24, 7, 31, 18, 6, 5, 3, 255, 255]
+        orderData[4] = (byte) 0x01;
+        orderData[5] = (byte) 0x01;
+        orderData[6] = (byte) 0xFF;
+        orderData[7] = (byte) 0xFF;
+//         FF 6 2 6 1 1 ff ff
     }
 
     @Override
