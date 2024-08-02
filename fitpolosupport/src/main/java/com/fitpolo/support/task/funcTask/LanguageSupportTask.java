8ddf3f4447d5c10bbbd690b7f7210f836b7520d3
rohn <878443170@qq.com>
2024-08-02 14:35:32 +0800

    feat：增加语言支持和时间校准

--- fitpolosupport/src/main/java/com/fitpolo/support/task/funcTask/LanguageSupportTask.java
+++ fitpolosupport/src/main/java/com/fitpolo/support/task/funcTask/LanguageSupportTask.java
@@ -12,32 +12,24 @@ import com.fitpolo.support.utils.DigitalConver;
 import java.util.Arrays;
 
 /**
- * 同步时间
+ * 语言支持
  */
-public class TimeAlignTask extends OrderTask {
+public class LanguageSupportTask extends OrderTask {
 
-    private static final int ORDERDATA_LENGTH = 14;
+    private static final int ORDERDATA_LENGTH = 6;
 
     private byte[] orderData;
 
-    public TimeAlignTask(MokoOrderTaskCallback callback) {
-        super(OrderType.WRITE, OrderEnum.syncTime, callback, OrderTask.RESPONSE_TYPE_WRITE_NO_RESPONSE);
+    public LanguageSupportTask(MokoOrderTaskCallback callback) {
+        super(OrderType.WRITE, OrderEnum.languageSupport, callback, OrderTask.RESPONSE_TYPE_WRITE_NO_RESPONSE);
         orderData = new byte[ORDERDATA_LENGTH];
         orderData[0] = (byte) MokoConstants.HEADER_READ_SEND;
-        orderData[1] = (byte) 0x0C;
+        orderData[1] = (byte) 0x04;
         orderData[2] = (byte) 0x02;
         orderData[3] = (byte) order.getOrderHeader();
-        orderData[4] = (byte) 0x07;
-        orderData[5] = (byte) 0x18;
-        orderData[6] = (byte) 0x07;
-        orderData[7] = (byte) 0x1F;
-        orderData[8] = (byte) 0x12;
-        orderData[9] = (byte) 0x06;
-        orderData[10] = (byte) 0x05;
-        orderData[11] = (byte) 0x03;
-        orderData[12] = (byte) 0xFF;
-        orderData[13] = (byte) 0xFF;
-//        [255, 12, 2, 1, 7, 24, 7, 31, 18, 6, 5, 3, 255, 255]
+        orderData[4] = (byte) 0xFF;
+        orderData[5] = (byte) 0xFF;
+//      FF 4 2 7 FF FF
     }
 
     @Override
@@ -52,7 +44,12 @@ public class TimeAlignTask extends OrderTask {
         if (order.getOrderHeader() != DigitalConver.byte2Int(value[3])) {
             return;
         }
+
+        int group = DigitalConver.byte2Int(value[5]);
+        if (group != 0)
+            return;
         orderStatus = OrderTask.ORDER_STATUS_SUCCESS;
+
         MokoSupport.getInstance().pollTask();
         callback.onOrderResult(response);
         MokoSupport.getInstance().executeTask(callback);
